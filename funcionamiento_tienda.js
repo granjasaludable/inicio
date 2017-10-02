8bf76039a74b1a9be66231df6bf7927e54a185a4
granjasaludable <32280314+granjasaludable@users.noreply.github.com>
2017-10-02 05:15:12 +0200

    Add files via upload

--- funcionamiento_tienda.js
+++ funcionamiento_tienda.js
@@ -2,48 +2,49 @@
 
 var volverPosicionScroll=0;	
 var menuActivadoDesactivado=false;
+var cancelarRetardo;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////EMPIEZA CÓDIGO QUE FUNCIONA PARA MOSTRAR Y OCULTAR EN UN MISMO BOTÓN//////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function(){
 "use strict";
-		$("#menu").click(function () {
-      		$('#fondoCabecera').each(function() {
-				var menuActivarDesactivar = $(this).css("position");
-		  
-        		if(menuActivarDesactivar === "fixed") {
-           			$(this).css("position","absolute");
-					
-					menuActivadoDesactivado=true;
-					
-					volverPosicionScroll=window.scrollY;			//PARA SITUAR EL
-					$("html, body").animate({scrollTop:"0px"}, 0);	// SCROLL DONDE ESTABA
-					$("#contenedorCuerpo").css("display","none");
-				}
+	$("#menu").click(function () {
+      	$('#fondoCabecera').each(function() {
+			var menuActivarDesactivar = $(this).css("position");
+	  
+       		if(menuActivarDesactivar === "fixed") {
+          			$(this).css("position","absolute");
 				
-				else {
-					$(this).css("position","fixed");
-					
-					menuActivadoDesactivado=false;
-					$("#contenedorCuerpo").css("display","flex");
+				menuActivadoDesactivado=true;
 				
-					$("html, body").animate({scrollTop:volverPosicionScroll}, 0);	//PARA SITUAR EL
-					volverPosicionScroll=0;											// SCROLL DONDE ESTABA
-        		}
-			});
+				volverPosicionScroll=window.scrollY;			//PARA SITUAR EL
+				$("html, body").animate({scrollTop:"0px"}, 0);	// SCROLL DONDE ESTABA
+				$("#contenedorCuerpo").css("display","none");
+			}
 			
-			$('nav').each(function() {
-				var menuActivarDesactivar = $(this).css("display");
+			else {
+				$(this).css("position","fixed");
 				
-        		if(menuActivarDesactivar === "block") {
-           			$(this).css("display","none");
-        		}
-				
-				else {
-            		$(this).css("display","block");
-        		}
-			});
+				menuActivadoDesactivado=false;
+				$("#contenedorCuerpo").css("display","flex");
+			
+				$("html, body").animate({scrollTop:volverPosicionScroll}, 0);	//PARA SITUAR EL
+				volverPosicionScroll=0;											// SCROLL DONDE ESTABA
+       		}
 		});
+			
+		$('nav').each(function() {
+			var menuActivarDesactivar = $(this).css("display");
+			
+       		if(menuActivarDesactivar === "block") {
+          			$(this).css("display","none");
+       		}
+			
+			else {
+           		$(this).css("display","block");
+       		}
+		});
+	});
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////TERMINA CÓDIGO QUE FUNCIONA PARA MOSTRAR Y OCULTAR EN UN MISMO BOTÓN//////////////////
@@ -63,25 +64,48 @@ function mostrarOcultarBarra(){
 	
 	var anchoVentana=window.innerWidth;
 	
+	
+	
+	//////////ARREGLO PARA EL DESPLEGABLE DE LA TIENDA///////////
+	if(anchoVentana >= 768) {
+		$('#desplegableSuperior').css("position","absolute");
+		
+		var tiendaActivarDesactivar = $('#desplegableSuperior').css("visibility");
+		
+		if(tiendaActivarDesactivar === "visible") {
+			$('#desplegableSuperior').css("visibility","hidden");
+			$('#desplegableSuperior').css("height",0);
+		}
+	}										
+	
+	else {
+		$('#desplegableSuperior').css("position","static");
+	}
+	//////////ARREGLO PARA EL DESPLEGABLE DE LA TIENDA///////////
+	
+	
+	
 	if(anchoVentana >= 769) {
+		
 		$('#fondoCabecera').css("position","static");
 		$('nav').css("display","block");
 		$("#contenedorCuerpo").css("display","flex");
 		
 		menuActivadoDesactivado=false;		//SI QUITO ESTO AL VOLVER DE ANCHO A
-	}										//ESTRECHO SIGUE ABIERTO EL DESPLEGABLE
+											//ESTRECHO SIGUE ABIERTO EL DESPLEGABLE
+		
+		
 	
+	}										
 	
 	else if (anchoVentana < 769 && menuActivadoDesactivado===false) {
 		$('#fondoCabecera').css("position","fixed");
 		$('nav').css("display","none");
-		$('.nav_div_ul_li_ul').css("display","none");
 	}
 	
 	else {
 		$('#fondoCabecera').css("position","absolute");
 		$('nav').css("display","block");
-		$('.nav_div_ul_li_ul').css("display","none");
 	}
 }
 
@@ -98,87 +122,57 @@ window.onresize=mostrarOcultarBarra;
 
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////EMPIEZA CÓDIGO QUE ARREGLA DESPLEGABLE TIENDA POR EL ANCHO CON SCRIPT//////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////EMPIEZA CÓDIGO QUE HACE DESPLEGABLE TIENDA POR EL ANCHO CON SCRIPT//////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function(){
 "use strict";
-	
-	alert("Función lista esperando");
-	
-		$("#idLinkTienda").click(function () {
-			
-			alert("Ha entrado en el click");
-			
-      		var anchoPantallaParaTienda = window.innerWidth;
-			
-			alert(anchoPantallaParaTienda);
-			
-			if(anchoPantallaParaTienda<769){
-				alert("Ha entrado en el if");
 		
-				$('.nav_div_ul_li_ul').each(function() {
-					var tiendaActivarDesactivar = $(this).css("display");
-					if(tiendaActivarDesactivar === "block") {
-						$(this).css("display","none");
-						alert("Tienda desactivada");
-					}
-					else {
-						$(this).css("display","block");
-						alert("Tienda activada");
-					}
-				});
-			}
-		});
-		$("#idLinkTienda").hover(function () {
-			
-			alert("Ha entrado en el hover");
-			
-      		var anchoPantallaParaTienda = window.innerWidth;
-			
-			alert(anchoPantallaParaTienda);
-			
-			if(anchoPantallaParaTienda>=769){
-				alert("Ha entrado en el if");
-				
-				$('.nav_div_ul_li_ul').each(function() {
-					$(this).css("display","block");
-						alert("Tienda activada");
-				});
-			};
-		},
-		function(){
-			alert("Ha salido del hover");
-			
-			var anchoPantallaParaTienda = window.innerWidth;
-			
-			alert(anchoPantallaParaTienda);
-			
-			if(anchoPantallaParaTienda>=769){
-				alert("Ha entrado en el if");
-				
-				$('.nav_div_ul_li_ul').each(function() {
-					$(this).css("display","none");
-						alert("Tienda desactivada");
-				});
-			}
+	$("#idTienda").click(function () {
+      	var anchoPantallaParaTienda = window.innerWidth;
 		
-		});
+		if(anchoPantallaParaTienda<769){
+			var tiendaActivarDesactivar = $('#desplegableSuperior').css("visibility");
+		
+			if(tiendaActivarDesactivar === "visible") {
+				$('#desplegableSuperior').css("visibility","hidden");
+				$('#desplegableSuperior').stop(true).animate({height:0},500);
+			}
+			
+			else {
+				$('#desplegableSuperior').css("visibility","visible");
+				$('#desplegableSuperior').stop(true).animate({height:255},500);
+			}
+		}
+	});
+	
+	$("#idTienda").mouseenter(function () {
+      	var anchoPantallaParaTienda = window.innerWidth;
+		
+		if(anchoPantallaParaTienda>=768){
+		
+				cancelarRetardo=setTimeout(function() {
+					$('#desplegableSuperior').css("visibility","visible");
+					$('#desplegableSuperior').stop(true).animate({height:167},500);
+				},1000);
+				
+		}
+	});
+	
+	$("#idTienda").mouseleave(function () {
+		var anchoPantallaParaTienda = window.innerWidth;
+		
+		if(anchoPantallaParaTienda>=768){
+		
+			$('#desplegableSuperior').css("visibility","hidden");
+			$('#desplegableSuperior').stop(true).animate({height:0},0);
+			clearTimeout(cancelarRetardo);
+		}
+	});
 });
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////TERMINA CÓDIGO QUE ARREGLA DESPLEGABLE TIENDA POR EL ANCHO CON SCRIPT//////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-//$(document).ready(function(){
-//	"use strict";
-//			$("#idLinkTienda").click(function () {
-//				$('#nav_div_ul_li_ul').slideToggle();
-//			});
-//	});
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////TERMINA CÓDIGO QUE HACE DESPLEGABLE TIENDA POR EL ANCHO CON SCRIPT//////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -187,8 +181,6 @@ $(document).ready(function(){
 //CÓDIGO PARA MOSTRAR RESOLUCIÓN DE PANTALLA CON UN CLICK (PUEDE SER ÚTIL)
 $(document).ready(function(){
 "use strict";
-	
-
 	
 	$("#idLinkBolsa").click(
 		function(){	
